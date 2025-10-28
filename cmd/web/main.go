4cef6fe5537d7e3d5b1e3d0df73b00ff93e654a2
0x-singularity <garrettmcguire1@proton.me>
2025-10-27 20:52:07 -0400

    page 107, DB issue

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Define an application struct to hold the application wide dependencies (like the structured logger)
@@ -15,10 +18,19 @@ type application struct {
 func main() {
 	//Define a new command line flag, defaults to 4000 if not specified
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Introducing structured logging, writes to stdout and nil as second parameter keeps default settings
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	//Initialize new instance of the application struct that contains the logger
 	app := &application{
@@ -41,8 +53,23 @@ func main() {
 	//use the info() method to log the start server message at "info" severity
 	logger.Info("starting server", slog.String("addr", ":4000"))
 
-	err := http.ListenAndServe(*addr, mux)
+	err = http.ListenAndServe(*addr, mux)
 	// Use Error() method to log any error message returned by http.ListenAndServe() at Error severity
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
